0d8df335596426aa37690c318b486c13e690b0d1
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-06-23 17:04:10 +0530

    setup frontend for project

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl text-shadow-lg text-shadow-zinc-200 text-blue-900 font-black">
+        This is URL Shortener
+      </h1>
+    </div>
+  );
+};
+
+export default App;
