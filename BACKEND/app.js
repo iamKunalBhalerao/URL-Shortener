c74e24f967460628f23b696079ca5c79f1972f2d
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-06-25 02:44:48 +0530

    Completed Signup and Signin frontend

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -8,7 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(cookieParser());
 app.use(attachUser);
 
